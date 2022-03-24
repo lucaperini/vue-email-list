0c4d93115dba6cf2765cd000b50708d802f63972
lucaperini <luca.perini@gmail.com>
2022-03-24 17:33:44 +0100

    correzione consegna

--- js/main.js
+++ js/main.js
@@ -3,12 +3,9 @@ const app = new Vue({
     el : '#app',
 
     data : {
-        counter : 0,
+        
         generatedEmail : '',
         emailArray : [],
-        
-        
-
     },
 
     methods : {
@@ -16,98 +13,15 @@ const app = new Vue({
     },
 
     mounted(){
+        
         const self = this;
+        for (let i =0; i<10; i++){
         axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then (function (answer){
-            
-                self.generatedEmail= answer.data.response;
-                self.emailArray.push(answer.data.response);
-                console.log (self.emailArray)
-                
-            
-        });
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then (function (answer){
-            
-                self.generatedEmail= answer.data.response;
-                self.emailArray.push(answer.data.response);
-                console.log (self.emailArray)
-                
-            
-        });
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then (function (answer){
-            
-                self.generatedEmail= answer.data.response;
-                self.emailArray.push(answer.data.response);
-                console.log (self.emailArray)
-                
-            
-        });
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then (function (answer){
-            
-                self.generatedEmail= answer.data.response;
-                self.emailArray.push(answer.data.response);
-                console.log (self.emailArray)
-                
-            
-        });
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then (function (answer){
-            
-                self.generatedEmail= answer.data.response;
-                self.emailArray.push(answer.data.response);
-                console.log (self.emailArray)
-                
-            
-        });
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then (function (answer){
-            
-                self.generatedEmail= answer.data.response;
-                self.emailArray.push(answer.data.response);
-                console.log (self.emailArray)
-                
-            
-        });
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then (function (answer){
-            
-                self.generatedEmail= answer.data.response;
-                self.emailArray.push(answer.data.response);
-                console.log (self.emailArray)
-                
-            
-        });
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then (function (answer){
-            
-                self.generatedEmail= answer.data.response;
-                self.emailArray.push(answer.data.response);
-                console.log (self.emailArray)
-                
-            
-        });
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then (function (answer){
-            
-                self.generatedEmail= answer.data.response;
-                self.emailArray.push(answer.data.response);
-                console.log (self.emailArray)
-                
-            
-        });
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then (function (answer){
-            
-                self.generatedEmail= answer.data.response;
-                self.emailArray.push(answer.data.response);
-                console.log (self.emailArray)
-                
-            
-        });
-
+            .then (function (answer){
+            self.generatedEmail= answer.data.response;
+            self.emailArray.push(answer.data.response);
+            });
+        }
     }
     
 });
